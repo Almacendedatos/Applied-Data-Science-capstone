4396cb9af05f13649d9b001f2386c7f5e6962ab7
Raul Tejedor <82455607+Almacendedatos@users.noreply.github.com>
2021-10-31 14:25:07 +0100

    Create Content_to_list.py

---
+++ Content_to_list.py
@@ -0,0 +1,21 @@
+# import of libraries
+import os
+
+# Request for data
+path = input(" directory path :")
+name = input(" Nome for the file :")
+
+completeName = os.path.join(path, name+".txt")   
+
+# obtain list of files and directories into the results file
+
+
+with open(completeName, 'w') as f:
+  for root, dirs, files in os.walk(path):
+    for i in files:
+        f.write("%s\n" % i)
+    for j in dirs:
+        f.write("%s\n" % j)
+        
+      
+ 
